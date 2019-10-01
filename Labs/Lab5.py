d9e679950c305ca86881fd682c45f6427951423c
cofflahj <55641937+cofflahj@users.noreply.github.com>
2019-09-30 21:56:26 -0400

    Update Lab5.py

--- Labs/Lab5.py
+++ Labs/Lab5.py
@@ -1,10 +1,12 @@
-from random import randint
-def guess(x):
-  it = randint(0, 101)
-  if x == it:
-      print("You got it!")
-  elif x > it:
-      print("too high")
+str1 = input("Please enter the first word that comes to mind after reading this: ")
+vowels = 0
+consonants = 0
+
+for i in str1:
+  if(i == 'a' or i == 'e' or i == 'o' or i == 'u' or i == 'A' or i == 'E' or i == 'I' or i == 'O' or i == 'U'):
+    vowels = vowels + 1
   else:
-      print("too low")
-print guess(50)
+    consonants = consonants + 1
+    
+print("Total # of Vowels: ", vowels)
+print("Total # of Consonants: ", consonants)
